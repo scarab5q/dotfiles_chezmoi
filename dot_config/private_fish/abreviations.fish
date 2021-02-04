08f5e411e61bc6945f32ddd5c400d4199a3b260b
scarab5q <jackmdenny@gmail.com>
2021-02-04 11:36:15 +0000

    add fish abbreviations file

---
+++ dot_config/private_fish/abreviations.fish
@@ -0,0 +1,121 @@
+abbr ls 'exa'
+abbr nnl 'nn -l'
+abbr gsp 'cd ~/google_drive/Side_Projects/'
+abbr s3 'cd ~/google_drive/Sheffield/3rdYear/'
+abbr s3aa 'cd ~/google_drive/Sheffield/3rdYear/Advanced_Algorithms_COM3105'
+abbr s3ai 'cd ~/google_drive/Sheffield/3rdYear/Adaptive_Inteligence_COM3240'
+abbr s3s 'cd ~/google_drive/Sheffield/3rdYear/Computer_Security_and_Forensics_COM3501'
+abbr s3iw 'cd  ~/google_drive/Sheffield/3rdYear/Inteligent_Web_COM3504'
+abbr diss 'cd ~/google_drive/Sheffield/3rdYear/Dissertation'
+abbr s3ds 'cd ~/google_drive/Sheffield/3rdYear/Distributed_Systems_COM3190'
+abbr jnot 'jupyter notebook'
+# abbr notify-send 'dunstify'
+abbr config ' /run/current-system/sw/bin/git --git-dir $HOME/.cfg/ --work-tree $HOME'
+abbr ncmpcpp 'ncmpcpp -b /home/jack/.config/ncmpcpp/bindings'
+abbr randWal "wal -i ~/google_drive/Wallpaper/"
+#abbr ec "emacsclient -c -n -a ''"
+# abbr ll "ls -l | awk '{print $NF}'"
+abbr rangerConfig "~/.config/ranger"
+abbr nvimConfig "~/.SpaceVim.d/init.toml"
+abbr i3config "~/.config/i3/config"
+abbr refresh_wallpaper "$SCRIPTS/changing_wallpaper.sh"
+abbr ys "yay -Ss"
+abbr yrns "yay -Rns"
+abbr yr "yay -R"
+abbr yi "yay -Sy"
+abbr yin "yay -Sy --noconfirm"
+abbr yu "yay -Syyu"
+abbr yun "yay -Syyu --noconfirm"
+abbr comptonConfig "~/.config/compton.conf"
+abbr termiteConfig "~/.config/termite/config"
+abbr sA "source activate"
+abbr .. "cd ../"
+abbr ... "cd ../.."
+abbr .... "cd ../../.."
+abbr ..... "cd ../../../.."
+abbr ...... "cd ../../../../.."
+abbr ....... "cd ../../../../../.."
+abbr ........ "cd ../../../../../../.."
+abbr ......... "cd ../../../../../../../.."
+abbr .......... "cd ../../../../../../../../.."
+abbr ........... "cd ../../../../../../../../../.."
+abbr ............ "cd ../../../../../../../../../../.."
+abbr ............. "cd ../../../../../../../../../../../.."
+abbr .............. "cd ../../../../../../../../../../../../.."
+abbr ............... "cd ../../../../../../../../../../../../../.."
+abbr ................ "cd ../../../../../../../../../../../../../../.."
+abbr ................. "cd ../../../../../../../../../../../../../../../.."
+abbr .................. "cd ../../../../../../../../../../../../../../../../.."
+abbr ................... "cd ../../../../../../../../../../../../../../../../../.."
+abbr .................... "cd ../../../../../../../../../../../../../../../../../../.."
+abbr ..................... "cd ../../../../../../../../../../../../../../../../../../../.."
+abbr ...................... "cd ../../../../../../../../../../../../../../../../../../../../.."
+abbr ....................... "cd ../../../../../../../../../../../../../../../../../../../../../.."
+abbr ........................ "cd ../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ......................... "cd ../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr .......................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ........................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ............................ "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ............................. "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr .............................. "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ............................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ................................ "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ................................. "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr .................................. "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr .................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ..................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ...................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ....................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ........................................ "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ......................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr .......................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ........................................... "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr ............................................ "cd ../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../.."
+abbr vpnSheff "sudo vpnc sheffield --local-port 0"
+abbr vpnSheffKill "sudo vpnc-disconnect"
+abbr franz "/home/jack/Downloads/franz-5.0.0-beta.16-x86_64.AppImage"
+abbr rmt "rmtrash"
+abbr tensorflow "source ~/envs/tensorflow/bin/activate"
+abbr zshc "clear && zsh"
+abbr klp "killall -p"
+abbr efc 'f -e emacsclient -c'
+abbr etc 'f -e emacsclient -t'
+abbr c 'fasd_cd -d'
+abbr projects_rust "cd /home/jack/google_drive/Projects/rust"
+abbr todov 'n Dropbox/todo/todo.txt'
+abbr lsa "ls -a"
+abbr k 'kak'
+abbr aria \'aria2c\'
+abbr antibodyr 'antibody bundle < ~/.zsh/.zsh_plugins.txt > ~/.zsh/.zsh_plugins.sh'
+abbr nrs
+
+abbr "\$" ""
+abbr bsprefresh '~/.config/bspwm/bspwmrc'
+abbr gc 'git commit -v'
+abbr gca 'git commit -va'
+abbr cgc 'config commit -v'
+abbr cgca 'config commit -va'
+abbr vf 'vifm'
+abbr config '/usr/bin/env git --git-dir $HOME/.cfg/ --work-tree $HOME'
+abbr lazyc 'lazygit --git-dir $HOME/.cfg/ --work-tree $HOME'
+abbr zshc "clear && source ~/.zshrc"
+# abbr c 'fasd_cd -d'
+abbr pomodoro \'i3-gnome-pomodoro\'
+abbr dotdrop \'/home/jack/dotdrop/dotdrop.sh\'
+abbr n $EDITOR
+abbr nrs 'sudo nixos-rebuild switch --show-trace'
+abbr nrso 'sudo nixos-rebuild switch --show-trace --option substitute false'
+abbr nrsu 'sudo nixos-rebuild switch --show-trace --upgrade'
+abbr ns 'nix search'
+abbr nsu 'nix search -u'
+abbr nip 'nix-env -iP myPackages'
+abbr nipr 'nix-env -iPr myPackages'
+abbr nip "nix-env -iA myPackages -f '<nixpkgs>'"
+# abbr nip 'nix-env -iP myPackages'
+abbr proj 'cd ~/google_drive/projects'
+abbr wfica "/opt/Citrix/ICAClient/wfica"
+abbr notes "cd /home/jack/google_drive/notes"
+abbr books "cd /home/jack/google_drive/Books"
+# XBPS
+abbr xisu "sudo xbps-install -Su"

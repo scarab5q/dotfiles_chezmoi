b508de48cd59d11610c14d762909601d275ce6b9
scarab5q <jackmdenny@gmail.com>
2021-02-06 14:08:17 +0000

    add TODOs to chezmoi.el

--- dot_doom.d/chezmoi.el
+++ dot_doom.d/chezmoi.el
@@ -19,10 +19,15 @@
 ;;
 ;;; Code:
 
+;; TODO make a function to find the destination of the source file
 (defun is_managed_by_chezmoi (&optional file)
-  (or file (setq file (buffer-file-path)))
-  (memq ))
+  (or file (setq file (buffer-file-path))))
 
+;; TODO make a function to ask whether you want to actually edit the sourcefile
+;; TODO recreate chezmoi merge with ediff
+;; TODO make a function to ask if you want to appy this source file
+;; TODO make a function to show all files that have changed with diffs and then ask if you want to apply all the files
+;; TODO auto create a diff between the source and destination files
 
 (provide 'chezmoi)
 ;;; chezmoi.el ends here

ca11ef77a083963dc21646247e9a5af9397ea105
scarab5q <jackmdenny@gmail.com>
2021-02-09 11:51:36 +0000

    format nixos/machines/desktop.nix

--- nixos/machines/desktop.nix
+++ nixos/machines/desktop.nix
@@ -2,57 +2,36 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config
-, pkgs
-, dunstify
-, ...
-}:
+{ config, pkgs, dunstify, ... }:
 
-let
-  dwm-HEAD =
-    pkgs.callPackage
-    ./dwm
-    { };
+let dwm-HEAD = pkgs.callPackage ./dwm { };
 
 in {
-  imports =
-    [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
-      ../common.nix
-    ];
+  imports = [ # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    ../common.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
 
-  networking.hostName =
-    "nix-desktop-box"; # Define your hostname.
-  networking.hostId =
-    "0502641e";
-  environment =
-    {
+  networking.hostName = "nix-desktop-box"; # Define your hostname.
+  networking.hostId = "0502641e";
+  environment = {
 
-      # system packages
-      systemPackages =
-        with pkgs;
-        [
+    # system packages
+    systemPackages = with pkgs;
+      [
 
-        ];
-    };
+      ];
+  };
 
   # Enable  the X11 windowing system.
-  services =
-    {
+  services = {
 
-      xserver =
-        {
-          xrandrHeads =
-            [
-              "HDMI-1"
-              "DP-2"
-            ];
-        };
-      # Enable touchpad support.
-      # libinput.enable = true;
-    };
+    xserver = { xrandrHeads = [ "HDMI-1" "DP-2" "DP-1" ]; };
+    # Enable touchpad support.
+    # libinput.enable = true;
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database

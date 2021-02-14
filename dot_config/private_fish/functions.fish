ea338693edba875e7c77b95696fe3578ddea0190
scarab5q <jackmdenny@gmail.com>
2021-02-14 18:51:42 +0000

    fix functions.fish file

--- dot_config/private_fish/functions.fish
+++ dot_config/private_fish/functions.fish
@@ -1,6 +1,6 @@
 function brightnessup
 	if [ -z $argv[1] ]
-	then
+
 		light -A 5
 	else
 		light -A $argv[1]
@@ -9,7 +9,7 @@ end
 
 function brightnessdown
 	if [ -z $argv[1] ]
-	then
+
 		light -U 5
 	else
 		light -U $argv[1]
@@ -28,11 +28,6 @@ function volumemute
   pactl set-sink-mute 0 toggle
 end
 
-function fe
-  set IFS $'\n' files=((fzf-tmux --query="$argv[1]" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
-end
-
 function ccd
   cd (chezmoi source-path)
 end
@@ -41,9 +36,9 @@ function cef
   set file (chezmoi managed -i files | sed "s/.*chezmoi//g" |fzf )
   if set -q $file
     chezmoi edit $file
-    echo "do you want to chezmoi apply?\ntype y|Y|yes|Yes to apply\ntype n|N|no|No|nO|N|<Enter> to not apply"
+    echo "do you want to chezmoi apply?"
     read response
-    if string match -r 'y|Y|yes|Yes' $response >/dev/null then
+    if string match -r 'y|Y|yes|Yes' $response
       echo applying
       chezmoi apply
     else 
@@ -52,33 +47,6 @@ function cef
   end
 end
 
-function e
-  if [ $# -eq 0 ]; then
-      set F ( fasd -lRf | fzf ) || exit 1
-      if [ -f $F ];then
-          $EDITOR $F
-      end
-  end
-end
-
-# function nixify
-#   if [ ! -e ./.envrc ]; then
-#     echo "use nix" > .envrc
-#     direnv allow
-#   end
-#   if [ ! -e default.nix ]; then
-#     cat > default.nix <<'EOF'
-# with import <nixpkgs> {};
-# stdenv.mkDerivation {
-#   name = "env";
-#   buildInputs = [
-#     bashInteractive
-#   ];
-# }
-# EOF
-#     ${EDITOR:-vim} default.nix
-#   end
-# end
 
 function ftpane
   local panes current_window current_pane target target_window target_pane
@@ -91,10 +59,10 @@ function ftpane
   set target_window (echo $target | awk 'BEGIN{FS=":|-"} {print$argv[1]}')
   set target_pane (echo $target | awk 'BEGIN{FS=":|-"} {print$argv[2]}' | cut -c 1)
 
-  if [[ $current_window -eq $target_window ]]; then
-    tmux select-pane -t ${target_window}.${target_pane}
+  if [[ $current_window -eq $target_window ]];
+    tmux select-pane -t $target_window.$target_pane
   else
-    tmux select-pane -t ${target_window}.${target_pane} &&
+    tmux select-pane -t $target_window.$target_pane &&
     tmux select-window -t $target_window
   end
 end
@@ -103,80 +71,73 @@ function cinto
     cat <'EOF' >>$argv[1]
 end
 
-function fkill
-    local pid
-    if [ "$UID" != "0" ]; then
-        set pid (ps -f -u $UID | sed 1d | fzf -m | awk '{print $argv[2]}')
-    else
-        set pid (ps -ef | sed 1d | fzf -m | awk '{print $argv[2]}')
-    end
-    if [ "x$pid" != "x" ]
-    then
-        echo $pid | xargs kill -${1:-9}
-    end
-end
+# function fkill
+#     if [ "$UID" != "0" ];
+#         set pid (ps -f -u $UID | sed 1d | fzf -m | awk '{print $argv[2]}')
+#     else
+#         set pid (ps -ef | sed 1d | fzf -m | awk '{print $argv[2]}')
+#     end
+#     if set -q $pid
+#         echo $pid | xargs kill -${1:-9}
+#     end
+# end
 
 
-# function to use path extractor. A tool that extracts paths from stdout
-function pe 
-    path-extractor
-end
+# # function to use path extractor. A tool that extracts paths from stdout
+# function pe
+#     path-extractor
+# end
 
-# a function to list all directories and have them filterable and searchable
-function lff 
-    ls -a1 | fzf
-end
+# # a function to list all directories and have them filterable and searchable
+# function lff
+#     ls -a1 | fzf
+# end
 
-# a function to change directories using fzf
-function cz 
-    set d (fasd -dlR | fzf)
-    printf "%s %s\n" "cd" "$d" >> ~/.zhistory
-    cd $d
-end
+# # a function to change directories using fzf
+# function cz
+#     set d (fasd -dlR | fzf)
+#     printf "%s %s\n" "cd" "$d" >> ~/.zhistory
+#     cd $d
+# end
 
-function cf 
-    cd ( fd -t d | fzf )
-end
+# function cf
+#     cd ( fd -t d | fzf )
+# end
 
-# a function to change directories using fzf
-function cF 
-    cd ( fd . -t d ~ | fzf )
-end
+# # a function to change directories using fzf
+# function cF
+#     cd ( fd . -t d ~ | fzf )
+# end
 
-# a function to change directories using fzf
-function cfa 
-    cd ( fd -t d --hidden | fzf )
-end
+# # a function to change directories using fzf
+# function cfa
+#     cd ( fd -t d --hidden | fzf )
+# end
 
-# a function to change to any directory searched from the home directory
-function cFA 
-    cd ( fd . -t d --hidden ~ | fzf )
-end
+# # a function to change to any directory searched from the home directory
+# function cFA
+#     cd ( fd . -t d --hidden ~ | fzf )
+# end
 
-function fr 
-    $argv[1] ( sk --ansi -i -c "rg set --color always -i --line-number --hidden '{} .'" --preview "preview.sh {}"  )
-end
+# function fr
+#     $argv[1] ( sk --ansi -i -c "rg set --color always -i --line-number --hidden '{} .'" --preview "preview.sh {}"  )
+# end
 
 # fg () {
 #     $argv[1] (exa -a1r | sk --ansi)
 # }
 
-# ll () {
-#     exa -l | awk '{print $NF}'
-# }
+ll () {
+    exa -l | awk '{print $NF}'
+}
 
-function gclone 
-    if [ $# -eq 0 ]; then
-        git clone
-    end
-end
 
 
 # function ea 
-#     if [  -eq 0 ]; then
+#     if [  -eq 0 ];
 #         set file  ( fd -t f --hidden | sk) || return 1
 #         $EDITOR $file
-#     elif [ $argv[1] = '-f' ]; then
+#     elif [ $argv[1] = '-f' ];
 #         set file  ( fd -t f --hidden | fzy) || return 1
 #         $EDITOR $file
 #     end
